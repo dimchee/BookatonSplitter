b19a2bda8613032e18645bbb7b8581351c8fdbb2
dimchee <dimchee90@gmail.com>
2023-03-06 23:57:58 +0100

    Add GH file explorer

--- src/Main.elm
+++ src/Main.elm
@@ -2,13 +2,11 @@ module Main exposing (..)
 
 import Browser
 import Dict exposing (Dict)
-import File exposing (File)
-import File.Select
 import Html
 import Html.Attributes
 import Html.Events
+import Http
 import Json.Decode
-import Task
 
 
 type alias Percentage =
@@ -22,23 +20,48 @@ type alias Img =
     }
 
 
+type alias File =
+    { name : String
+    , url : String
+    }
+
+
+type alias Path =
+    List File
+
+
+type alias Dir =
+    { path : Path
+    , files : Maybe (Dict String File)
+    }
+
+
+type alias Repo =
+    String
+
+
+rootFile : Repo -> File
+rootFile repo =
+    { name = "/"
+    , url = "https://api.github.com/repos/" ++ repo ++ "/git/trees/master"
+    }
+
+
 type alias Model =
     { percentage : Percentage
     , images : Dict String Img
+    , repo : Repo
+    , curDir : Dir
     }
 
 
 type Msg
     = ChangedPos Percentage
     | Split
-    | ImagesRequested
-    | ImagesLoaded File (List File)
     | GotUrl Img String
-
-
-requestImages : Cmd Msg
-requestImages =
-    File.Select.files [ "image/png", "image/jpeg" ] ImagesLoaded
+    | RepoChanged String
+    | GotDirListing (Maybe (Dict String File))
+    | Selected String
 
 
 gradient : Percentage -> String
@@ -65,10 +88,21 @@ viewImages =
             ]
 
 
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { percentage = 0
+      , images = Dict.empty
+      , repo = "makccr/wallpapers"
+      , curDir = { path = [], files = Nothing }
+      }
+    , listDir <| rootFile "makccr/wallpapers"
+    )
+
+
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \_ -> ( { percentage = 0, images = Dict.empty }, Cmd.none )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
@@ -76,13 +110,13 @@ main =
 
 
 view : Model -> Html.Html Msg
-view { percentage, images } =
+view { percentage, images, curDir } =
     Html.div
         [ Html.Attributes.style "display" "flex"
         , Html.Attributes.style "flex-direction" "column"
         ]
         [ Html.img
-            [ Html.Attributes.src "https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/art/ARTWORK-bridal-procession-on-the-hardangerfjord.jpg"
+            [ Html.Attributes.src <| "https://raw.githubusercontent.com/makccr/wallpapers/master/wallpapers/art/ARTWORK-bridal-procession-on-the-hardangerfjord.jpg"
             , Html.Attributes.height 500
             , Html.Attributes.style "-webkit-mask-image" <| gradient percentage
             , Html.Attributes.style "mask-image" <| gradient percentage
@@ -91,14 +125,19 @@ view { percentage, images } =
             , Html.Events.onMouseOut <| ChangedPos 0
             ]
             []
-        , Html.button [ Html.Events.onClick ImagesRequested ] [ Html.text "Choose Files" ]
+
+        -- , Html.button [ Html.Events.onClick ImagesRequested ] [ Html.text "Choose Files" ]
+        , Html.input [ Html.Events.onInput RepoChanged, Html.Attributes.placeholder "Github Repository" ] []
         , viewImages images
+        , Html.div [] [ Html.text <| "Selected Path: " ++ (curDir.path |> List.map .name |> List.reverse |> String.join "/") ]
+        , Html.div []
+            [ List.map
+                (Html.option [] << List.singleton << Html.text)
+                (curDir.files |> Maybe.map Dict.keys |> Maybe.withDefault [])
+                |> List.append [ Html.option [] [ Html.text "." ], Html.option [] [ Html.text ".." ] ]
+                |> Html.select [ Html.Events.onInput Selected ]
+            ]
         ]
-
-
-fileToImg : File -> Img
-fileToImg file =
-    { name = File.name file, mime = File.mime file, url = Nothing }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -110,25 +149,61 @@ update msg model =
         Split ->
             ( model, Cmd.none )
 
-        ImagesRequested ->
-            ( model, requestImages )
-
-        ImagesLoaded img imgs ->
-            ( { model
-                | images =
-                    img
-                        :: imgs
-                        |> List.map (\x -> ( File.name x, fileToImg x ))
-                        |> Dict.fromList
-              }
-            -- TODO Should be lazy (Don't load all images at once)
-            , List.map (\file -> File.toUrl file |> Task.perform (GotUrl <| fileToImg file)) (img :: imgs) |> Cmd.batch
-            )
-
         GotUrl img url ->
             ( { model | images = Dict.insert img.name { img | url = Just url } model.images }
             , Cmd.none
             )
+
+        RepoChanged repo ->
+            ( { model | repo = repo }
+            , listDir <| rootFile repo
+            )
+
+        GotDirListing files ->
+            ( { model | curDir = { path = model.curDir.path, files = files } }
+            , Cmd.none
+            )
+
+        Selected name ->
+            if name == ".." then
+                let
+                    newPath =
+                        List.tail model.curDir.path |> Maybe.withDefault [ rootFile model.repo ]
+                in
+                ( { model
+                    | curDir = { path = newPath, files = Nothing }
+                  }
+                , newPath |> List.head |> Maybe.withDefault (rootFile model.repo) |> listDir
+                )
+
+            else
+                case Maybe.andThen (Dict.get name) model.curDir.files of
+                    Just file ->
+                        ( { model
+                            | curDir = { path = file :: model.curDir.path, files = Nothing }
+                          }
+                        , listDir <| file
+                        )
+
+                    Nothing ->
+                        ( model, Cmd.none )
+
+
+listDir : File -> Cmd Msg
+listDir { url } =
+    Http.get
+        { url = url
+        , expect =
+            Json.Decode.field "tree"
+                (Json.Decode.list <|
+                    Json.Decode.map2 File
+                        (Json.Decode.field "path" Json.Decode.string)
+                        (Json.Decode.field "url" Json.Decode.string)
+                )
+                |> Json.Decode.map (List.map (\({ name } as file) -> ( name, file )) >> Dict.fromList)
+                |> Http.expectJson
+                    (Result.toMaybe >> GotDirListing)
+        }
 
 
 mouseMoveDecoder : Json.Decode.Decoder Msg
