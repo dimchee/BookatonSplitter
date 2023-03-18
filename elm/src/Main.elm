3e57f3884c46699d5231de676daf00cb52020d2e
dimchee <dimchee90@gmail.com>
2023-03-18 20:44:45 +0100

    Cleanup

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -25,11 +25,15 @@ type Split
     | Middle Percentage
 
 
+type alias Splits =
+    Dict Img Split
+
+
 type alias Model =
     { percentage : Percentage
     , page : Int
     , pageIncStep : Int
-    , modifications : Dict Img Split
+    , splits : Splits
     , serverMsg : Maybe String
     }
 
@@ -42,10 +46,10 @@ type Direction
 type Msg
     = ChangedPos Percentage
     | ChangedPageIncStep (Maybe Int)
-    | VisitModification Direction
+    | VisitSplit Direction
     | VisitPage Direction
     | Modify Split
-    | RemoveModification
+    | RemoveSplit
     | Step
     | Save
     | Saved (Result Http.Error String)
@@ -90,13 +94,13 @@ splitButton last text split =
         [ Html.text text ]
 
 
-modificationLine : Model -> Html.Html msg -> Html.Html msg
-modificationLine { page, modifications } img =
+splitLine : Model -> Html.Html msg -> Html.Html msg
+splitLine { page, splits } img =
     Html.div
         [ Html.Attributes.style "position" "relative"
         , Html.Attributes.style "width" "min-content"
         ]
-        [ case Dict.get page modifications of
+        [ case Dict.get page splits of
             Just (Middle p) ->
                 Html.span
                     [ Html.Attributes.style "position" "absolute"
@@ -121,13 +125,13 @@ modificationLine { page, modifications } img =
 
 
 viewImage : Model -> Html.Html Msg
-viewImage ({ percentage, page, modifications } as model) =
+viewImage ({ percentage, page, splits } as model) =
     Html.div
         [ Html.Attributes.style "width" "min-content"
         , Html.Attributes.style "height" "100%"
         ]
-        [ splitButton (Dict.get page modifications) "Split Before" Before
-        , modificationLine model <|
+        [ splitButton (Dict.get page splits) "Split Before" Before
+        , splitLine model <|
             Html.img
                 [ Html.Attributes.src <| "images/" ++ String.fromInt page
                 , Html.Attributes.style "-webkit-mask-image" <| gradient percentage
@@ -139,7 +143,7 @@ viewImage ({ percentage, page, modifications } as model) =
                 , Html.Attributes.height imageHeight
                 ]
                 []
-        , splitButton (Dict.get page modifications) "Split After" After
+        , splitButton (Dict.get page splits) "Split After" After
         ]
 
 
@@ -148,9 +152,63 @@ imageHeight =
     1000
 
 
+numOfPages : Int
+numOfPages =
+    -- TODO make this vvalue part of model (idealy through init)
+    1128
+
+
 navButton : String -> Msg -> Html.Html Msg
 navButton text msg =
     Html.button [ Html.Attributes.style "font-size" "2em", Html.Events.onClick msg ] [ Html.text text ]
+
+
+viewPageNum : Model -> Html.Html Msg
+viewPageNum { page } =
+    Html.div
+        [ Html.Attributes.style "display" "flex"
+        , Html.Attributes.style "flex-direction" "column"
+        ]
+        [ Html.div [ Html.Attributes.style "font-size" "2em" ]
+            [ Html.text "Page ", numSpan (page + 1), Html.text " of ", numSpan numOfPages ]
+        , Html.div []
+            [ navButton "Last" <| VisitPage Last
+            , navButton "Next" <| VisitPage Next
+            , navButton "Step" <| Step
+            ]
+        ]
+
+
+viewSplitNum : Model -> Html.Html Msg
+viewSplitNum { splits, page } =
+    Html.div
+        [ Html.Attributes.style "display" "flex"
+        , Html.Attributes.style "flex-direction" "column"
+        ]
+        [ Html.div [ Html.Attributes.style "font-size" "2em" ]
+            [ Html.text "Split "
+            , Dict.keys splits
+                |> List.Extra.findIndex ((<) page)
+                |> Maybe.withDefault (Dict.size splits)
+                |> numSpan
+            , Html.text " of "
+            , numSpan <| Dict.size splits
+            ]
+        , Html.div []
+            [ navButton "Last" <| VisitSplit Last
+            , navButton "Next" <| VisitSplit Next
+            , navButton "Del" RemoveSplit
+            ]
+        ]
+
+
+numSpan : Int -> Html.Html Msg
+numSpan num =
+    Html.span
+        [ Html.Attributes.style "width" "2em"
+        , Html.Attributes.style "display" "inline-block"
+        ]
+        [ Html.text <| String.fromInt num ]
 
 
 view : Model -> Html.Html Msg
@@ -167,26 +225,19 @@ view ({ pageIncStep, serverMsg } as model) =
             , Html.Attributes.style "justify-content" "center"
             , Html.Attributes.style "text-align" "center"
             ]
-            [ navButton "Remove Change" RemoveModification
-            , navButton "Last Change" <| VisitModification Last
-            , navButton "Next Change" <| VisitModification Next
-            , navButton "Last Page" <| VisitPage Last
-            , navButton "Next Page" <| VisitPage Next
-            , navButton "Step" <| Step
+            [ viewSplitNum model
+            , viewPageNum model
             , Html.div []
-                [ Html.input
+                [ Html.text "Step size "
+                , numSpan pageIncStep
+                , Html.input
                     [ Html.Attributes.type_ "range"
                     , Html.Attributes.min "1"
-                    , Html.Attributes.max "20"
+                    , Html.Attributes.max "30"
                     , Html.Attributes.value <| String.fromInt pageIncStep
                     , Html.Events.onInput (String.toInt >> ChangedPageIncStep)
                     ]
                     []
-                , Html.span
-                    [ Html.Attributes.style "width" "4em"
-                    , Html.Attributes.style "font-size" "2em"
-                    ]
-                    [ Html.text <| String.fromInt pageIncStep ]
                 ]
             , Html.div []
                 [ navButton "Save" Save
@@ -197,12 +248,11 @@ view ({ pageIncStep, serverMsg } as model) =
         ]
 
 
-closestModFiltered : (Int -> Bool) -> Model -> Int
-closestModFiltered isGood { page, modifications } =
-    modifications
+closestSplitFiltered : (Int -> Bool) -> Model -> Int
+closestSplitFiltered isGood { page, splits } =
+    splits
         |> Dict.keys
-        -- TODO clamp index (total images not known for now)
-        -- |> (\l -> (page + pageIncStep) :: l)
+        -- |> (\l -> (clamp 0 (numOfPages - 1) <| page + pageIncStep) :: l)
         |> List.filter isGood
         |> List.Extra.minimumBy (\ind -> abs <| ind - page)
         |> Maybe.withDefault page
@@ -228,31 +278,29 @@ update msg model =
             ( Maybe.map (\x -> { model | pageIncStep = x }) m |> Maybe.withDefault model, Cmd.none )
 
         Step ->
-            -- TODO clip index from above
-            ( { model | page = model.page + model.pageIncStep }, Cmd.none )
+            ( { model | page = clamp 0 (numOfPages - 1) <| model.page + model.pageIncStep }, Cmd.none )
 
         VisitPage dir ->
-            -- TODO clip index from above
-            ( { model | page = max 0 <| model.page + chooseLastNext dir 1 -1 }, Cmd.none )
+            ( { model | page = clamp 0 (numOfPages - 1) <| model.page + chooseLastNext dir -1 1 }, Cmd.none )
 
-        VisitModification dir ->
-            ( { model | page = closestModFiltered (chooseLastNext dir (>) (<) model.page) model }, Cmd.none )
+        VisitSplit dir ->
+            ( { model | page = closestSplitFiltered (chooseLastNext dir (>) (<) model.page) model }, Cmd.none )
 
         Modify percentage ->
             { model
-                | modifications = Dict.update model.page (\_ -> Just percentage) model.modifications
+                | splits = Dict.update model.page (\_ -> Just percentage) model.splits
             }
                 |> update Step
 
-        RemoveModification ->
+        RemoveSplit ->
             ( { model
-                | modifications = Dict.remove model.page model.modifications
+                | splits = Dict.remove model.page model.splits
               }
             , Cmd.none
             )
 
         Save ->
-            ( model, save model.modifications )
+            ( model, save model.splits )
 
         Saved result ->
             ( { model | serverMsg = Result.toMaybe result }, Cmd.none )
@@ -265,8 +313,8 @@ update msg model =
                 |> Maybe.map
                     (\m ->
                         ( { model
-                            | modifications = m
-                            , serverMsg = Just <| "Loaded " ++ String.fromInt (Dict.size m) ++ " modifications"
+                            | splits = m
+                            , serverMsg = Just <| "Loaded " ++ String.fromInt (Dict.size m) ++ " splits"
                           }
                         , Cmd.none
                         )
